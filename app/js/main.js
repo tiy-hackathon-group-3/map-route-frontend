f2d1e0f7eb5f29d0f948b98823bb496ccf3fc5d6
Michael Marcinek <michaelmarcinek@gmail.com>
2015-06-26 21:10:53 -0400

    Initializes git and sets up project

---
+++ app/js/main.js
@@ -0,0 +1,17 @@
+;(function (){
+
+  'use strict';
+
+  angular.module('ngRoute', 'uiGmapgoogle-maps', [
+
+    function (ngRoute, uiGmapgoogle-maps)
+
+
+
+    ])
+
+}());
+
+//save to inject into map view:
+
+// <ui-gmap-google-map center='map.center' zoom='map.zoom'></ui-gmap-google-map>
