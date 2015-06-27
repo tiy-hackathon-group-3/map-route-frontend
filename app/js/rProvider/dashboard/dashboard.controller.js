46030353958a903d72593fc277b14683ff0f7e9a
robert fine <robertfine4631@icloud.com>
2015-06-27 19:10:07 -0400

    made list of trip names

--- app/js/rProvider/dashboard/dashboard.controller.js
+++ app/js/rProvider/dashboard/dashboard.controller.js
@@ -4,19 +4,27 @@
 
   angular.module('rProvider')
 
-  .controller('Dashboard', ['$scope', '$http', function ($scope, $http){
+  .controller('Dashboard', ['$scope', '$http', 'HEROKU', function ($scope, $http, HEROKU){
 
   	//Display Username on dashboard
 
   	$scope.user = Cookies.get('username');
 
+
+
   	//Creating function and Method for retrieving and pushing routes to dash
 
-  console.log($http.get('https://nameless-forest-6621.herokuapp.com/trips/:trip-id/locations'));
+  // console.log($http.get('https://nameless-forest-6621.herokuapp.com/trips', HEROKU.CONFIG));
 
 
-
-
+  	$http.get('https://nameless-forest-6621.herokuapp.com/trips', HEROKU.CONFIG)
+  		.success( function(data){
+  			console.log(data);
+  			$scope.trips = data;
+  		})
+  		.error( function(err){
+  			console.error(err);
+  		});
 
 
 
