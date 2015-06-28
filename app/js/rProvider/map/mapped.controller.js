2f9db9c31ae24df3fab5daee3b53f3fc8ba72dd4
Michael Marcinek <michaelmarcinek@gmail.com>
2015-06-27 21:50:30 -0400

    Gets the MAP!!!!

--- app/js/rProvider/map/mapped.controller.js
+++ app/js/rProvider/map/mapped.controller.js
@@ -4,14 +4,21 @@
 
   angular.module('rProvider')
 
-  .controller('Mapped', ['$scope', 'UserService',
+  .controller('Mapped', ['$scope' , 'UserService',
     function ($scope, UserService) {
 
       console.log('Hello from Map Controller');
 
       UserService.tokenizeHeader();
 
+      var map = L.map('map').setView([33.7622229802915, -84.33107501970849], 13);
 
+      L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWljaGFlbG1hcmNpbmVrIiwiYSI6ImIyMjRmOTdlYzIzZTU0MmI4MWYwYmVhYTNmMWQ2NGNmIn0.Sd0s189TLjNqC1LjSAktFg', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        maxZoom: 18,
+        id: 'michaelmarcinek.mijfd45l',
+        accessToken: 'your.mapbox.public.access.token'
+      }).addTo(map);
     }
 
 
